1e1e95a0fea73c4ce00af663470e87174fa4f5ab
Yarik Brouwer <i362569@fhict.nl>
2023-05-15 20:18:23 +0200

    Added project for mongodb

---
+++ ImageUploadService/Services/IImageService.cs
@@ -0,0 +1,13 @@
+﻿using ImageUploadService.Model;
+
+namespace ImageUploadService.Services
+{
+    public interface IImageService
+    {
+        List<Image> Get();
+        Image Get(string id);
+        Image Create(Image image);
+        void Update(string id, Image image);
+        void Remove(string id);
+    }
+}
---
+++ ImageUploadService/Services/ImageService.cs
@@ -0,0 +1,41 @@
+﻿using ImageUploadService.Model;
+using MongoDB.Driver;
+
+namespace ImageUploadService.Services
+{
+    public class ImageService : IImageService
+    {
+        private readonly IMongoCollection<Image> _images;
+
+        public ImageService(IImageDbSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName = "test");
+            _images = database.GetCollection<Image>(settings.ImageCollectionName = "Images");
+        }
+        public Image Create(Image image)
+        {
+            _images.InsertOne(image);
+            return image;
+        }
+
+        public List<Image> Get()
+        {
+            return _images.Find(image => true).ToList();
+        }
+
+        public Image Get(string id)
+        {
+            return _images.Find(image => image.id == id).FirstOrDefault();
+        }
+
+        public void Remove(string id)
+        {
+            _images.DeleteOne(image => image.id == id);
+        }
+
+        public void Update(string id, Image image)
+        {
+            _images.ReplaceOne(student => student.id == id, image);
+        }
+    }
+}
