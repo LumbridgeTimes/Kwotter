65a571fb6cc32c97fc3e6b4f52a285345c395c72
Yarik Brouwer <i362569@fhict.nl>
2023-05-17 12:02:42 +0200

    Added Serverless computing from Azure
    Like counter now published to Azure

---
+++ ServerlessFollowerCounter/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServerlessFollowerCounter.Pages
+{
+    public class IndexModel : PageModel
+    {
+public int Counter { get; set; }
+
+        public void OnGet()
+        {
+            Counter = 0;
+        }
+
+        public IActionResult OnPost()
+        {
+            Counter++;
+            return Page();
+        }
+    }
+}
